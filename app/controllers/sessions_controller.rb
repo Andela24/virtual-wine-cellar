e560866e7e0b8c2393296a7100f778d6524e9852
Andela Stevanovic <andjela.s166@gmail.com>
2022-10-12 12:02:00 -0400

    fixed authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,14 @@
 class SessionsController < ApplicationController
     # login - creating a new session (not user)
     def create
-      @user = User.find_by(username: params[:username])
+      user = User.find_by(username: params[:username])
      
       # authenticate is a new method given to us by Bcrypt. It checks the password passed in if it's the correct password
-      if @user 
-        login_user
-        render json: @user, status: :ok
+      
+        if user&.authenticate(params[:password])
+          session[:user_id] = user.id
+          render json: user, status: :created
+      
       else
         render json: { errors: ["Username or Password did not match."] }
       end
