c8ffb25773bc6162cec72168959e54869f24cc45
Andela Stevanovic <andjela.s166@gmail.com>
2022-10-17 17:41:09 -0400

    created main component

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -8,7 +8,7 @@ class NavBar extends React.Component {
         <Navbar.Brand href="/">Virtual Wine Cellar</Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="/login">Login</Nav.Link>
-          <Nav.Link href="/logout">Logout</Nav.Link>
+          <Nav.Link href="/logout">Sign Up</Nav.Link>
           
         </Nav>
       </Navbar>
---
+++ client/src/containers/Main.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import NavBar from '../components/pageComponents/NavBar'
+import HomePageContainer from './HomePageContainer'
+import WineriesContainer from './WineriesContainer'
+import CellarContainer from './CellarContainer'
+import NotFound from '../components/pageComponents/NotFound'
+
+class Main extends Component{
+
+    state = {
+        name: "Chase Hilbert"
+        
+    }
+
+
+render() {
+    return (
+        <div className="main">
+            <Router>
+                <NavBar />
+                <Switch>
+                    <Route exact path="/" component={HomePageContainer} />
+                    <Route exact path='/cellar' render={(props) => <CellarContainer {...props} user={this.state.name} />} />
+                    <Route exact path="/wineries" component={WineriesContainer} />
+                    <Route exact path="/bottles" />
+                    <Route component={NotFound} />
+                </Switch>
+            </Router>
+        </div>
+        )
+    }
+}
+
+export default Main
